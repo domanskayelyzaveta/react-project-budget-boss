f14cb68afa09ec1765d00fe1b526ae5549af134f
Valentan390 <137200405+Valentan390@users.noreply.github.com>
2023-10-31 20:35:59 +0200

    Add Auth

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from 'redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/react-project-budget-boss">
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter basename="/react-project-budget-boss">
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
--- src/redux/userReducer.js
+++ src/redux/userReducer.js
@@ -1,7 +1,38 @@
+const { createSlice } = require('@reduxjs/toolkit');
+const { registerThunk } = require('./thunks');
+
 const initialState = {
   userData: null,
   isLoading: false,
   error: null,
-  token: null,
+  accessToken: null,
   isSignedIn: false,
+  id: null,
 };
+
+const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  extraReducers: builder => {
+    builder
+      //------------ Registration ---------------
+
+      .addCase(registerThunk.pending, state => {
+        state.error = null;
+        state.isLoading = true;
+      })
+      .addCase(registerThunk.fulfilled, (state, action) => {
+        state.error = null;
+        state.isLoading = false;
+        state.isSignedIn = true;
+        state.id = action.payload.id;
+        state.userData = action.payload.user;
+      })
+      .addCase(registerThunk.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.payload;
+      });
+  },
+});
+
+export const userReducer = userSlice.reducer;
