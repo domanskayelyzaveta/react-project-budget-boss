6a8084a7ae575836b86b303a3f517d2e04feb3ab
Aleksandr2905 <natalukha@ukr.net>
2023-11-01 14:17:42 +0200

    period switch

---
+++ src/components/TotalStatistic/TotalStatistics.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TotalStatistics = () => {
+  return (
+    <div>TotalStatistics</div>
+  )
+}
+
+export default TotalStatistics;
--- src/pages/Reports/ReportsPage.jsx
+++ src/pages/Reports/ReportsPage.jsx
@@ -1,9 +1,11 @@
 import CategoriesList from 'components/CategoriesList/CategoriesList';
+import PeriodSwitch from 'components/PeriodSwitch/PeriodSwitch';
 import React from 'react';
 
 const Reports = () => {
   return (
     <div>
+      <PeriodSwitch/>
       <CategoriesList />
     </div>
   );
