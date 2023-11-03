544d9b3903cb44f9018db929f639581533335820
domanskayelyzaveta <domanskaya_@ukr.net>
2023-11-03 20:38:23 +0100

    date-pick-css-create

--- src/components/DatePicker/StyledDatePicker.styled.js
+++ src/components/DatePicker/StyledDatePicker.styled.js
@@ -1,7 +1,7 @@
 import { createGlobalStyle, styled } from 'styled-components';
 export const DataBtn = styled.div`
   padding: 6px 12px;
-  color: #ffffff;
+  color: #fff;
   background: #3e85f3;
   border-radius: 8px;
   font-weight: 700;
@@ -9,6 +9,7 @@ export const DataBtn = styled.div`
   line-height: 18px;
   text-align: center;
   text-transform: uppercase;
+
   @media screen and (min-width: 768px) {
     margin-top: 33px;
     margin-bottom: 33px;
@@ -19,26 +20,30 @@ export const DataBtn = styled.div`
 export const CalendarGlobalStyles = createGlobalStyle`
   .react-datepicker__wrapper {
     position: relative;
-
   }
+
   .react-datepicker {
     font-size:10px;
     position: absolute;
-    left: 50%;
-    top: 50%;
+    // left: 50%;
+    // top: 150%;
     transform: translate(-1%, 0%);
     display: flex;
     flex-direction: column;
     align-items: center;
-    width: 327px;
-    height: 354px;
-    padding: 9px 18px;
-  background-color: var(--button);
-    font-family: Inter;
-    border-radius: 16px;
+    width: 242px;
+    height: 258px;
+    padding-top: 22px;
+    padding-right: 22px;
+    padding-left: 22px;
+    padding-bottom: 10px;
+    border-radius: 30px;
+    border: none;
+    background: linear-gradient(118deg, #58BB67 3.05%, #368342 125.62%);
   }
   .react-datepicker__month-container {
     float: inherit;
+     width: 242px;
     box-sizing: border-box;
   }
   .react-datepicker__input-container input{
@@ -46,13 +51,13 @@ export const CalendarGlobalStyles = createGlobalStyle`
   }
   .react-datepicker__header {
     position: relative;
-    background-color: var(--button);
+   background: linear-gradient(118deg, #58BB67 -4%, #368342 180.62%);
   }
   .react-datepicker__day-names {
     display: flex;
+    font-size: 12px;
     justify-content: space-between;
     align-items: center;
-    padding: 8px 8px;
     border-bottom: 1px solid rgba(255, 255, 255, 0.4);
   }
   .react-datepicker__header:not(.react-datepicker__header--has-time-select) {
@@ -64,41 +69,48 @@ export const CalendarGlobalStyles = createGlobalStyle`
     border: none;
 }
   .react-datepicker__current-month {
-    font-family: Inter;
-    font-size: 20px;
-    font-style: normal;
-    font-weight: 600;
-    line-height: 24px;
+    fort-size: 14px;
+    font-weight: 700;
+    font-family: Roboto;
+    letter-spacing: 0.28px;
+    margin-bottom: 18px;
+    line-height: 20px;
     text-align: center;
     color: white;
+    text-transform: uppercase;
   }
+
   .react-datepicker__day-name {
     margin: 0;
-    padding: 8px 8px;
-    font-family: Inter;
-    font-size: 14px;
+    font-size: 12px;
     font-style: normal;
-    font-weight: 600;
-    line-height: 18px;
+    font-family: Roboto;
+    font-weight: 700;
+    margin-bottom: 12px;
     text-align: center;
     color: white;
+    letter-spacing: 0.24px;
+    text-transform: uppercase;
   }
   .react-datepicker__navigation {
     margin-top: 24px;
     color: white;
+    color: white!important;
   }
   .react-datepicker__navigation--previous {
     left: 7px;
-    width: 18px;
+    width: 44px;
     height: 18px;
+    color: white!important;
   }
   .react-datepicker__navigation--next {
     right: 7px;
-    width: 18px;
+    width: 44px;
     height: 18px;
+    color: white!important;
   }
   .react-datepicker__navigation-icon::before:hover:focus {
-    border-color: white;
+    color: white!important;
   }
   .react-datepicker__week {
     display: flex;
@@ -107,35 +119,22 @@ export const CalendarGlobalStyles = createGlobalStyle`
     color: white;
   }
   .react-datepicker__day {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    gap: 8px;
-    flex-shrink: 0;
-    margin: 0;
-    width: 32px;
-    height: 32px;
+  
     border-radius: 50%;
-    color: white;
-    font-family: Inter;
-    font-size: 14px;
-    font-style: normal;
+    color: #F6F7FB70;
+    font-size: 12px;
     font-weight: 400;
-    line-height: 18px;
-    text-align: center;
+
   }
   .react-datepicker__month {
     display: flex;
-    gap: 7px;
+    margin-top: 16px;
     flex-direction: column;
     justify-content: space-between;
-    padding-top: 16px;
-    margin: 0;
   }
   .react-datepicker__day--selected {
     background-color:white;
-    color:#3e85f3;
+    color:#41964F;;
     font-size: 14px;
   }
   .react-datepicker__day--selected:hover {
@@ -152,9 +151,7 @@ export const CalendarGlobalStyles = createGlobalStyle`
     background-color:white;
     color:#3e85f3;
   }
-  .react-datepicker__day--weekend {
-    opacity: 50%;
-  }
+
 .react-datepicker__day--outside-month {
     background-color: transparent;
     pointer-events: none;
@@ -166,20 +163,33 @@ export const CalendarGlobalStyles = createGlobalStyle`
   .react-datepicker__triangle {
     visibility: hidden;
   }
+
+  // .react-datepicker__year-read-view--down-arrow, .react-datepicker__month-read-view--down-arrow, .react-datepicker__month-year-read-view--down-arrow, .react-datepicker__navigation-icon::before{
+  //   color: #fff!important;
+  // }
+
+  // .react-datepicker__navigation:hover *::before {
+  //  color: #fff!important;
+  // .react-datepicker__navigation:hover *::after {
+  //  color: #fff!important;
+  // }
 `;
 
 export const TitleWrapper = styled.button`
-  width: 195px;
-  color: #ffffff;
-  background:var(--background);;
-  border-radius: 8px;
-  font-weight: 700;
-  line-height: 18px;
-  text-align: center;
-  text-transform: uppercase;
-  padding: 8px 12px;
-  font-size: 16px;
-  justify-content: space-around;
-  align-items: center;
-  border:none;
+  background: transparent;
+  border: none;
+  width: 74px;
+  font-size: 12px;
+  font-weight: 900;
+  Line height: normal;
+  padding-left: 10px;
+  letter-spacing: 0.48px;
+  font-family: Roboto;
+  color: var(--white-DC);
+
+`;
+
+export const CalcWrapper = styled.div`
+  margin-top: 36px;
+  display: flex;
 `;
